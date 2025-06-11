63e2fc342dd39b7ba081a84515888b7037eca925
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-06-11 14:53:27 -0300

    Adiciona pasta components e routes e faz estrutura App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+import Home from "./routes/Home"
+import Footer from "./routes/Error"
+
 import "./index.css"
 
 function App() {
 
 
   return (
-    <>
-  
-    </>
+    <BrowserRouter>
+      <Nav/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
